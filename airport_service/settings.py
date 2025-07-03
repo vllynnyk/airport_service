1972ecc69efa8c3468e611b4e2a6efa4d14757ae
vllynnyk <magnetto54@gmail.com>
2025-07-03 20:07:37 +0200

    feat: configure DRF with pagination, permissions, JWT, schema and throttling

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -134,3 +134,24 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'airservice.permissions.IsAdminAllOrIsAuthenticatedReadOnly',
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+       "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_THROTTLE_CLASSES": [
+       "rest_framework.throttling.AnonRateThrottle",
+       "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+       "anon": "100/day",
+       "user": "1000/day"
+}
+}
+
