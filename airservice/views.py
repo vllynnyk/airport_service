65e4470db7bed429844ee44e164b5b0b2f6f7c40
vllynnyk <magnetto54@gmail.com>
2025-07-03 20:11:17 +0200

    feat: create view for Airport model

---
+++ airservice/views.py
@@ -0,0 +1,33 @@
+from django.db.models import Prefetch
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from airservice.models import Airport, Route
+from airservice.serializers import AirportSerializer, AirportListSerializer, AirportRetrieveSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == "retrieve":
+            queryset = queryset.prefetch_related(
+                Prefetch(
+                    "routes_from",
+                    queryset=Route.objects.select_related("destination")
+                ),
+                Prefetch(
+                    "routes_to",
+                    queryset=Route.objects.select_related("source")
+                ),
+            )
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirportListSerializer
+        elif self.action == "retrieve":
+            return AirportRetrieveSerializer
+        return AirportSerializer
