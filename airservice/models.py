f9edea1c1309cac350e5ec09664aeb114c308e27
vllynnyk <magnetto54@gmail.com>
2025-07-03 12:52:02 +0200

    feat: add Route model with __str__ method and default ordering

--- airservice/models.py
+++ airservice/models.py
@@ -19,3 +19,25 @@ class Airport(models.Model):
     def __str__(self):
         return f"{self.name}, closest city: {self.closest_big_city}"
 
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes_to"
+    )
+    distance = models.IntegerField()
+    display_name = models.CharField(max_length=255, editable=False, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["source", "destination"],
+                name="unique_route_source_destination"
+            )
+        ]
+        ordering = ("source", "destination")
+
+    def __str__(self):
+        return self.display_name
