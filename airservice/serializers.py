a178914626e3ef012a2542b3a2fdbf468c367056
vllynnyk <magnetto54@gmail.com>
2025-07-03 14:02:28 +0200

    feat: create serializer for detail Airplane model

--- airservice/serializers.py
+++ airservice/serializers.py
@@ -203,3 +203,16 @@ class FlightRetrieveSerializer(FlightSerializer):
             "crew"
         ]
 
+class AirplaneRetrieveSerializer(AirplaneSerializer):
+    flights = FlightListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Airplane
+        fields = [
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "airplane_type",
+            "flights"
+        ]
