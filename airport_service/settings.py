c851cf8712e4a478f520b18e190dfc6bc7fa4306
vllynnyk <magnetto54@gmail.com>
2025-07-03 20:08:53 +0200

    feat: configure JWT token lifetime and refresh settings

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -168,4 +168,8 @@ SPECTACULAR_SETTINGS = {
     },
 }
 
-
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+}
