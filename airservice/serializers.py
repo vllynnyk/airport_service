ac90d67a394c49b498230da0ebb24c2ef7be3787
vllynnyk <magnetto54@gmail.com>
2025-07-03 13:34:30 +0200

    feat: create base serializer for Route model with validate

--- airservice/serializers.py
+++ airservice/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airservice.models import Airport
+from airservice.models import Airport, Route
 
 
 class AirportSerializer(serializers.ModelSerializer):
@@ -13,3 +13,27 @@ class AirportListSerializer(AirportSerializer):
     class Meta:
         model = Airport
         fields = ["id", "name", "country"]
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ["id", "source", "destination", "distance"]
+
+    def validate(self, attrs):
+        source = (
+            attrs.get("source")
+            if "source" in attrs
+            else (self.instance.source if self.instance else None)
+        )
+        destination = (
+            attrs.get("destination")
+            if "destination" in attrs
+            else (self.instance.destination if self.instance else None)
+        )
+
+        if source is not None and destination is not None:
+            Route.validate_source_and_destination(
+                source, destination, serializers.ValidationError
+            )
+        return attrs
