2da822f45f17f615109e6268b9a0b87db02c8836
vllynnyk <magnetto54@gmail.com>
2025-07-06 12:42:14 +0200

    feat: associate created objects with authenticated user

--- airservice/views.py
+++ airservice/views.py
@@ -152,6 +152,9 @@ class OrderViewSet(
             queryset = queryset.prefetch_related("tickets__flight__crew")
         return queryset
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action == "list":
             return OrderListSerializer
