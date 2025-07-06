97db8905b434005f319f21c777f7a8995339d17e
vllynnyk <magnetto54@gmail.com>
2025-07-06 12:59:35 +0200

    feat: add OpenAPI schema and Swagger/Redoc UI endpoints

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -18,9 +18,13 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airservice/", include("airservice.urls", namespace="airservice")),
     path("api/user/", include("user.urls"), name="user"),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + debug_toolbar_urls()
